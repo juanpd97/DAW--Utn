a14e3b5d4a989d6f4e261ca0e8af36f1c387006b
juanpd97 <juanpdominguez97@gmail.com>
2024-10-27 18:41:39 -0300

    ok

--- model/Database.php
+++ model/Database.php
@@ -1,25 +1,35 @@
 <?php
 
+require_once __DIR__ . '/../init.php';
+
 class Database {
-    private $host = "localhost";
-    private $puerto = "3307";
-    private $dnNombre = "tp";
-    private $username = "root";
-    private $password = "";
+    private $host;
+    private $puerto;
+    private $dnNombre;
+    private $username;
+    private $password;
     
     private $conexion;
 
+    public function __construct() {
+        // Asignar las propiedades a partir de las constantes
+        $this->host = DB_HOST;
+        $this->puerto = DB_PORT;
+        $this->dnNombre = DB_NAME;
+        $this->username = DB_USER;
+        $this->password = DB_PASS;
+    }
 
     public function getConnection() {
         $this->conexion = null;
         try {
             $this->conexion = new PDO(
-                "mysql:host=" . $this->host.";port=". $this->puerto . ";dbname=" . $this->dnNombre,
+                "mysql:host=" . $this->host . ";port=" . $this->puerto . ";dbname=" . $this->dnNombre,
                 $this->username,
                 $this->password
             );
             $this->conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        } catch(PDOException $e) {
+        } catch (PDOException $e) {
             echo "Error de conexión: " . $e->getMessage();
         }
         return $this->conexion;
