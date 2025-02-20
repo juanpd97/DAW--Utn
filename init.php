6b385a74b80123bf6a3a46f4153d0a60bdcc6750
Jp <juanpdominguez97@gmail.com>
2025-02-20 10:52:59 -0300

    -

--- init.php
+++ init.php
@@ -1,8 +1,8 @@
 <?php
 
 define('DB_HOST', 'localhost');
-define('DB_PORT', '3307');
-define('DB_NAME', 'pruebaTpPhp');
+define('DB_PORT', '3306');
+define('DB_NAME', 'tp1php');
 define('DB_USER', 'root');
-define('DB_PASS', '');
+define('DB_PASS', 'root');
 
